fd5c70e650eaf8420837f22f512e7d6f4ace14d7
Vlad Polyakov <s9693118@gmail.com>
2024-04-24 18:00:35 +0300

    add order page

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './PlaceOrder.css'
+import { StoreContext } from '../../context/StoreContext'
 const PlaceOrder = () => {
+
+  const{getTotalCartAmount} = useContext(StoreContext)
   return (
-    <div>
-      
-    </div>
+    <form className='place-order'>
+      <div className="place-order-left">
+        <p className='title'>Delivery Information</p>
+        <div className="multi-field">
+          <input type="text" name="" id="" placeholder='First Name'/>
+          <input type="text" name="" id="" placeholder='Last Name'/>
+        </div>
+        <input type="text" placeholder='Email'/>
+        <input type="text" placeholder='Region'/>
+        <div className="multi-field">
+          <input type="text" name="" id="" placeholder='Street'/>
+          <input type="text" name="" id="" placeholder='House'/>
+        </div>
+        <div className="multi-field">
+          <input type="text" name="" id="" placeholder='Phone number'/>
+          <input type="text" name="" id="" placeholder='Apartment number'/>
+        </div>
+      </div>
+      <div className="place-order-right">
+      <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div>
+            <div className="cart-total-details">
+                <p>Subtotal</p>
+                <p>{getTotalCartAmount()}$</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+                <p>Delivery Fee</p>
+                <p>{2}$</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>{getTotalCartAmount()+2}$</b>
+            </div>
+          </div>
+          <button >Proceed to Payment</button>
+        </div>
+      </div>
+    </form>
   )
 }
 
