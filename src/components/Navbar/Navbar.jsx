cb442b551d9cb2ccdc86056e0488bcf1b516f1ba
Vlad Polyakov <s9693118@gmail.com>
2024-04-24 15:59:47 +0300

    popup login, creating account

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom';
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
 
     const [menu, setMenu] = useState("home");
 
@@ -22,7 +22,7 @@ const Navbar = () => {
                     <img src={assets.basket_icon} alt="" />
                     <div className='dot'></div>
                 </div>
-                <button>sing up</button>
+                <button onClick={()=>setShowLogin(true)}>sing up</button>
             </div>
         </div>
     )
